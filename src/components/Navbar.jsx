f235920c5904fe8629b0cef3492627f0dbea58cd
Teddy Fatim Diallo <teddyfatimdiallo@TFD.local>
2025-02-28 11:12:40 -0600

    Adjusted the navigation in navbar and set the local-post page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { use, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { AppBar, Toolbar, Button, Typography, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu"; // Import hamburger icon
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Box } from "@mui/material";
 
 function Navbar() {
-    const [selectedPage, setSelectedPage] = useState("News");
+    const location = useLocation();
+    const [selectedPage, setSelectedPage] = useState("");
 
-    const handleButtonClick = (pageName) => {
-        setSelectedPage(pageName);
-    };
+    useEffect(() => {
+        if (location.pathname === "/") setSelectedPage("News");
+        else if (location.pathname === "/local-posts") setSelectedPage("Local-Posts");
+        else if (location.pathname === "/profile") setSelectedPage("Account");
+    }, [location.pathname]);
 
     return (
         <AppBar position="fixed" sx={{ backgroundColor: 'white', minHeight: "40px" }} className="shadow-md">
@@ -27,21 +30,19 @@ function Navbar() {
                         color: selectedPage === "News" ? 'white' : 'black',
                         backgroundColor: selectedPage === "News" ? 'black' : 'transparent'
                     }}
-                    onClick={() => handleButtonClick("News")}
                 >
                     News
                 </Button>
 
                 <Button
                     component={Link}
-                    to="/profile"
+                    to="/local-posts"
                     sx={{
                         color: selectedPage === "Local-Posts" ? 'white' : 'black',
                         backgroundColor: selectedPage === "Local-Posts" ? 'black' : 'transparent'
                     }}
-                    onClick={() => handleButtonClick("Local-Posts")}
                 >
-                    Local-Posts
+                    Local Posts
                 </Button>
 
                 <Box sx={{ flexGrow: 1 }} />
@@ -58,7 +59,6 @@ function Navbar() {
                         color: selectedPage === "Account" ? 'white' : 'black',
                         backgroundColor: selectedPage === "Account" ? 'black' : 'transparent'
                     }}
-                    onClick={() => handleButtonClick("Account")}
                 >
                     Account
                 </Button>
---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const PostPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className = "max-w-3xl mx-auto mt-10 p-4 border rounded-lg shadow-md">
+                <h1 className = "text-2xl font-bold">Post a Local News</h1>
+                <form>
+                    {/*Form elements to be completed*/}
+                </form>
+            </div>
+        </div>
+    );
+};
+export default PostPage;
